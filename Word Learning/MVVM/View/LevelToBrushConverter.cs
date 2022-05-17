82d4aeb091101656eba21a9e9769fde7e30812c8
Łukasz Modzelewski <lukasz.modzelewski1@gmail.com>
2022-05-18 00:37:55 +0200

    Add main viewmodel and basic learning view

---
+++ Word Learning/MVVM/View/LevelToBrushConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Word_Learning.MVVM.View
+{
+    public class LevelToBrushConverter : IValueConverter
+    {
+        private static readonly LevelToBrushConverter instance = new LevelToBrushConverter();
+        public static LevelToBrushConverter Instance { get { return instance; } }
+
+        private LevelToBrushConverter() { }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) // throw new ArgumentException("Value is null.");
+                return DependencyProperty.UnsetValue;
+            var colorMap = new Color[] { // poziomy nauczenia słowa
+                Color.FromRgb(255/1, 255/4, 0), // czerwony, jeżeli użytkownik 0 razy z rzędu dobrze dopasował słowo do jego definicji
+                Color.FromRgb(255/2, 255/3, 0), // 1 raz
+                Color.FromRgb(255/3, 255/2, 0), // 2 razy
+                Color.FromRgb(255/4, 255/1, 0) // 3 razy
+            };
+            return new SolidColorBrush(colorMap[(int)value]);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Word Learning/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Word_Learning.Core;
+
+namespace Word_Learning.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand SwitchToLearning { get; private set; }
+
+        private object currentView;
+        public object CurrentView
+        {
+            get { return currentView; }
+            set { currentView = value; OnPropertyChanged(nameof(currentView)); }
+        }
+
+        public MainViewModel()
+        {
+            var learningVM = new LearningViewModel();
+            SwitchToLearning = new RelayCommand(o =>
+            {
+                CurrentView = learningVM;
+            });
+        }
+    }
+}
