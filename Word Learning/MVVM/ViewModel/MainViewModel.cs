437f7b6b164e9ce03fb045317a811ede370dd26f
Salomon1337 <salomonpl2000@gmail.com>
2022-05-18 11:59:17 +0200

    Add StatisticsView

--- Word Learning/MVVM/ViewModel/MainViewModel.cs
+++ Word Learning/MVVM/ViewModel/MainViewModel.cs
@@ -5,8 +5,9 @@ namespace Word_Learning.MVVM.ViewModel
     class MainViewModel : ObservableObject
     {
         public RelayCommand SwitchToLearning { get; private set; }
-
+        public RelayCommand SwitchToStatistics { get; private set; }    
         private object currentView;
+        public StatisticsViewModel StatisticsVM { get; set; }
         public object CurrentModeVM
         {
             get { return currentView; }
@@ -15,9 +16,11 @@ namespace Word_Learning.MVVM.ViewModel
 
         public MainViewModel()
         {
+            StatisticsVM = new StatisticsViewModel();
             var learningVM = new LearningViewModel();
             CurrentModeVM = learningVM;
             SwitchToLearning = new RelayCommand(o => CurrentModeVM = learningVM);
+            SwitchToStatistics = new RelayCommand(o => CurrentModeVM = StatisticsVM);
         }
     }
 }
