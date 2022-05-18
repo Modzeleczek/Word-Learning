2e9460240e4c2ade9729d7bca7205a555b255c52
Łukasz Modzelewski <lukasz.modzelewski1@gmail.com>
2022-05-18 19:13:39 +0200

    Add mockup word storage
    Make statistics viewmodel instance a local variable
    
    Add definition quiz view
    
    Upgrade word detail textboxes

---
+++ Word Learning/MVVM/ViewModel/DefinitionQuizViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using Word_Learning.Core;
+using Word_Learning.MVVM.Model;
+
+namespace Word_Learning.MVVM.ViewModel
+{
+    public class DefinitionQuizViewModel : ObservableObject
+    {
+        private Word word = null;
+        public Word Word
+        {
+            get { return word; }
+            set
+            {
+                word = value;
+                OnPropertyChanged(nameof(Word));
+            }
+        }
+        public string[] Answer { get; } = new string[4];
+        public RelayCommand[] AnswerClick { get; } = new RelayCommand[4];
+
+        public DefinitionQuizViewModel() { }
+
+        public void GenerateQuestion()
+        {
+            var wordsDB = WordStorage.Instance.Words;
+            Word = wordsDB[0];
+            Answer[0] = wordsDB[2].Word_;
+            Answer[1] = wordsDB[1].Word_;
+            Answer[2] = wordsDB[3].Word_;
+            Answer[3] = wordsDB[0].Word_;
+            int correctAnswer = 3;
+            for (int i = 0; i < 4; ++i)
+            {
+                int iCopy = i;
+                AnswerClick[i] = new RelayCommand(o =>
+                {
+                    if (iCopy == correctAnswer)
+                        MessageBox.Show("Correct answer.");
+                    else
+                        MessageBox.Show("Incorrect answer.");
+                });
+            }
+        }
+    }
+}
