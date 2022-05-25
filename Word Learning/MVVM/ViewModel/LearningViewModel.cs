3ec9b3ea70247a2608619556ff35b7bff389cadd
Łukasz Modzelewski <lukasz.modzelewski1@gmail.com>
2022-05-25 10:56:58 +0200

    Remove search text box style and add a custom control to get text binding to work

--- Word Learning/MVVM/ViewModel/LearningViewModel.cs
+++ Word Learning/MVVM/ViewModel/LearningViewModel.cs
@@ -24,10 +24,26 @@ namespace Word_Learning.MVVM.ViewModel
         }
         public WordDetailsViewModel CurrentWordVM { get; } = new WordDetailsViewModel();
 
+        private string searchText;
+        public string SearchText
+        {
+            get { return searchText; }
+            set
+            {
+                searchText = value;
+                OnPropertyChanged(nameof(SearchText));
+                Words.Clear();
+                foreach (var w in WordStorage.Instance.Words)
+                    if (w.Word_.Contains(searchText))
+                        Words.Add(w);
+                SelectedIndex = -1;
+            }
+        }
+
         public LearningViewModel()
         {
             Words = new ObservableCollection<Word>(WordStorage.Instance.Words);
-            SelectedIndex = -1;
+            SearchText = "";
         }
     }
 }
