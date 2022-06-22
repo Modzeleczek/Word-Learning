dc18aa6d31e46e49164f24cdaf36e3217aa0f095
Łukasz Modzelewski <lukasz.modzelewski1@gmail.com>
2022-06-22 13:27:05 +0200

    Add password box with hidden characters

--- Word Learning/MVVM/ViewModel/LoginViewModel.cs
+++ Word Learning/MVVM/ViewModel/LoginViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Security;
 using System.Windows;
+using System.Windows.Controls;
 using Word_Learning.Core;
 using Word_Learning.MVVM.Model;
 using Word_Learning.MVVM.View;
@@ -21,13 +23,6 @@ namespace Word_Learning.MVVM.ViewModel
             set { username = value; OnPropertyChanged(nameof(Username)); }
         }
 
-        private string password;
-        public string Password
-        {
-            get { return password; }
-            set { password = value; OnPropertyChanged(nameof(Password)); }
-        }
-
         public RelayCommand WindowLoaded { get; }
         public RelayCommand Login { get; private set; }
         public RelayCommand Register { get; private set; }
@@ -41,14 +36,18 @@ namespace Word_Learning.MVVM.ViewModel
             {
                 if (string.IsNullOrWhiteSpace(Username))
                 { Error("Specify a username."); return; }
-                if (!ValidatePassword()) return;
-                var status = Storage.AddUser(new User { Username = Username, Password = Password });
+                var box = (PasswordBox)e;
+                var password = box.Password;
+                if (!ValidatePassword(password)) return;
+                var status = Storage.AddUser(new User { Username = Username, Password = password });
                 if (status.Code == 0) Success("Please log in to your new account.");
                 else Error(status.Message);
             });
             Login = new RelayCommand(e =>
             {
-                var status = Storage.GetUser(Username, Password, out User user);
+                var box = (PasswordBox)e;
+                var password = box.Password;
+                var status = Storage.GetUser(Username, password, out User user);
                 if (status.Code == 0)
                 {
                     User.Instance = user;
@@ -58,18 +57,18 @@ namespace Word_Learning.MVVM.ViewModel
             });
         }
 
-        private bool ValidatePassword()
+        private bool ValidatePassword(string password)
         {
-            if (string.IsNullOrWhiteSpace(Password))
+            if (string.IsNullOrWhiteSpace(password))
             { Error("Specify a password."); return false; }
-            if (Password.Length < 8)
+            if (password.Length < 8)
             { Error("Password should be at least 8 characters long."); return false; }
             bool hasDigit = false;
-            foreach (var c in Password)
+            foreach (var c in password)
                 if (c >= '0' && c <= '9') { hasDigit = true; break; }
             if (!hasDigit) { Error("Password should contain at least one digit."); return false; }
             bool hasSpecial = false;
-            foreach (var c in Password)
+            foreach (var c in password)
                 if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')))
                 { hasSpecial = true; break; }
             if (!hasSpecial)
